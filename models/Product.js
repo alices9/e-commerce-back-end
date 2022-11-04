e3902ce0fb07c97f6d8b744065d9ad629e572eb3
Alice Shao <a1ice_shao@yahoo.com>
2022-11-03 21:44:55 -0400

    added requirements for Product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataType.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataType.DECIMAL,
+      allowNull: false,
+      // validates that the value is a decimal
+    },
+    stock: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      // default value of 10
+      // validates that value is numberic
+    },
+    category_id: {
+      type: DataType.INTEGER,
+      // references the category model's id
+    }
   },
   {
     sequelize,
