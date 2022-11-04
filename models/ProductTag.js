b388fc419ad5e45aeab02a99320f820ca5876d23
Alice Shao <a1ice_shao@yahoo.com>
2022-11-03 22:07:07 -0400

    added requirements for ProductTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataType.INTEGER,
+      // references the product model's id
+    },
+    tag_id: {
+      type: DataType.INTEGER,
+      // references the tag model's id
+    }
   },
   {
     sequelize,
